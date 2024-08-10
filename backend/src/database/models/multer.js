bc9da94218e7fd18b4955e66fe1cf00fa80cab52
Hareem <zainabhareemtayyab@gmail.com>
2024-08-10 18:06:18 +0500

    created multer

---
+++ backend/src/database/models/multer.js
@@ -0,0 +1,39 @@
+import multer from "multer";
+import path, { extname } from 'path';
+//import { uploadDir } from "./config.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const uploadDir = path.join(__dirname,  'upload');
+
+const storage = multer.diskStorage({
+    destination:function (req,file,cb){
+        cb(null, uploadDir );
+    },
+    filename:function(req,file,cb){
+        cb(null,`${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload= multer({
+    storage:storage,
+    limits:{
+        fileSize:1024*1024*5,
+    },
+    fileFilter:(req,file,cb)=>{
+        const fileTypes=/jpeg|jpg|png|gif/;
+        const extname=fileTypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = fileTypes.test(file.mimetype);
+         
+        if (mimetype && extname) {
+            return cb(null, true);
+        } else {
+            cb(new Error('Only images are allowed'));
+        }
+    }
+});
+
+export default upload;
