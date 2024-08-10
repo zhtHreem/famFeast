7743293a3586e7f41976ca5b464997634a61e745
Hareem <zainabhareemtayyab@gmail.com>
2024-08-10 18:07:18 +0500

    updated recipe schema

--- backend/src/database/models/Recipe/schema.js
+++ backend/src/database/models/Recipe/schema.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const recipeSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    ref: 'User',
+    required: true,
+  },
   name: {
     type: String,
     required: true,
@@ -25,8 +31,9 @@ const recipeSchema = new mongoose.Schema({
       required: true,
     }
   ],
-});
+  image: { type: String },
+}, { timestamps: true });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports = Recipe;
+export default Recipe;
