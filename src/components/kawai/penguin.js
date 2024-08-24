a7dd36c1dfcb9f974195a52fd075c05e252a83e8
Hareem <zainabhareemtayyab@gmail.com>
2024-08-24 20:19:09 +0500

    animated kawai

---
+++ src/components/kawai/penguin.js
@@ -0,0 +1,24 @@
+// Penguin.js
+import React from 'react';
+import './penguin.css'; // Import the CSS for the penguin
+
+const Penguin = () => {
+  return (
+   
+   
+    <div className="penguin">
+      <div className="cheek"></div>
+      <div className="cheek"></div>
+      <div className="eye"></div>
+      <div className="eye"></div>
+      <div className="beak"></div>
+      <div className="foot"></div>
+      <div className="foot"></div>
+      <div className="arm"></div>
+      <div className="arm"></div>
+    </div>
+    
+  );
+};
+
+export default Penguin;
