f5f281a287f888185d719fbe63a1a999d13bec82
Hareem <zainabhareemtayyab@gmail.com>
2024-08-25 17:32:46 +0500

    added cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 connectDB();
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-men-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json()); 
 
 const __filename = fileURLToPath(import.meta.url);
