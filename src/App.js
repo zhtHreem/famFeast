a13d180e062561b605c761002ba8edb2d04ba7c8
Hareem <zainabhareemtayyab@gmail.com>
2024-08-26 22:28:11 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
-import React,{ Suspense, useState } from 'react';
-import { Box } from '@mui/material';
+import React,{ useState } from 'react';
 import Header from './components/Header/header';
-import Biryani from '../src/images/Biryani.jpg'
-import footer from './components/Footer/footer';
 import Footer from './components/Footer/footer';
 import Navbar from './components/Header/navbar';
-import { Contrast } from '@mui/icons-material';
+
 
 
 
