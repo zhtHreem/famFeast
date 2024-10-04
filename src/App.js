5e32683f55b5034769eb7289674373609c84200d
Hareem Tayyab <44234547+zhtHreem@users.noreply.github.com>
2024-10-05 03:52:33 +0500

    removed lazy loading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import React,{ useState } from 'react';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import Navbar from './components/Header/navbar';
-
-
-
-
-const Recipes = React.lazy(() => import('./components/Header/recipies'));
+import Recipes from './components/Header/recipies';
 function App() {
   const [showLogin, setShowLogin] = useState(false);
 
